ca195c36793a336a022e783fa8170b14a8c98dc7
BalazsRigo <rigo.balazs86@gmail.com>
2024-11-04 21:47:27 +0100

    Add tasks to Kanban Board

--- dnd-tutorial/src/components/KanbanBoard.tsx
+++ dnd-tutorial/src/components/KanbanBoard.tsx
@@ -1,6 +1,6 @@
 import { useMemo, useState } from "react";
 import PlusIcon from "../icons/PlusIcon"
-import { Column, Id } from "../types";
+import { Column, Id, Task } from "../types";
 import ColumnContainer from "./ColumnContainer";
 import { DndContext, DragOverlay, DragStartEvent, useSensors, useSensor, PointerSensor } from "@dnd-kit/core";
 import { arrayMove, SortableContext } from "@dnd-kit/sortable";
@@ -10,6 +10,8 @@ function KanbanBoard() {
   const [columns, setColumns] = useState<Column[]>([]);
   const columnsId = useMemo(() => columns.map(col => col.id), [columns])
 
+  const [tasks, setTasks] = useState<Task[]>([]);
+
   const [activeColumn, setActiveColumn] = useState<Column | null>(null);
 
   const sensors = useSensors(useSensor(PointerSensor, {
@@ -17,7 +19,7 @@ function KanbanBoard() {
       distance: 30
     },
   })
-);
+  );
 
   return (
     <div className="
@@ -34,7 +36,14 @@ function KanbanBoard() {
           <div className="flex gap-4">
             <SortableContext items={columnsId}>
               {columns.map((col) => (
-                <ColumnContainer key={col.id} column={col} deleteColumn={deleteColumn} updateColumn={updateColumn} />
+                <ColumnContainer
+                  key={col.id}
+                  column={col}
+                  deleteColumn={deleteColumn}
+                  updateColumn={updateColumn}
+                  createTask={createTask}
+                  tasks={tasks.filter((task) => task.columnId === col.id)}
+                />
               ))}
             </SortableContext>
           </div>
@@ -66,6 +75,7 @@ function KanbanBoard() {
               column={activeColumn}
               deleteColumn={deleteColumn}
               updateColumn={updateColumn}
+              createTask={createTask}
             />
           )}
         </DragOverlay>, document.body
@@ -73,6 +83,15 @@ function KanbanBoard() {
       </DndContext>
     </div>
   );
+
+  function createTask(columnId: Id) {
+    const newTask: Task = {
+      id: generateId(),
+      columnId,
+      content: `Task ${tasks.length + 1}`,
+    };
+    setTasks([...tasks, newTask]);
+  }
 
   function createNewColumn() {
     const columnToAdd: Column = {
@@ -88,10 +107,10 @@ function KanbanBoard() {
     setColumns(filteredColumns);
   }
 
-  function updateColumn(id: Id, title: string){
-    const newColumns = columns.map((col)=> {
-      if(col.id !== id) return col;
-      return {...col, title};
+  function updateColumn(id: Id, title: string) {
+    const newColumns = columns.map((col) => {
+      if (col.id !== id) return col;
+      return { ...col, title };
     });
 
     setColumns(newColumns);
